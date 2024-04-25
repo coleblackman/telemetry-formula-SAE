12bfd716361653d0069dbc0a67259cd46298bae3
Cole Blackman <coleblax@gmail.com>
2024-04-25 00:00:38 -0400

    created GUI with drop down for tty device

---
+++ base_station/base_station.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import subprocess
+
+output = subprocess.check_output("ls /dev/tty*", shell=True)
+
+tty_devices = output.decode().splitlines()
+
+print(tty_devices)
+
+from PyQt5.QtWidgets import QComboBox, QMainWindow, QApplication, QWidget, QVBoxLayout
+from PyQt5.QtGui import QIcon
+import sys
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.ttyselector = QComboBox()
+        for device in tty_devices:
+            self.ttyselector.addItem(device)
+
+        self.ttyselector.currentIndexChanged.connect(self.index_changed)
+
+        layout = QVBoxLayout()
+        layout.addWidget(ttyselector)
+
+        combobox.currentIndexChanged.connect(self.index_changed)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+        def index_changed(self, index):
+            global selected_tty_device
+            selected_tty_device = tty_devices[index]
+            print("Selected TTY device:", selected_tty_device)
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec_()
