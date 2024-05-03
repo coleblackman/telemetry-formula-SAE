51fdcb9cc09a91ea01250752c3518f6ff25ec9d8
adamdirt <adam.dirting@gmail.com>
2024-05-03 13:08:46 -0400

    still testing

--- P2P_lora_formula/src/main.cpp
+++ P2P_lora_formula/src/main.cpp
@@ -1,11 +1,33 @@
 #include <RadioLib.h>
 #include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include "driver/temp_sensor.h"
 
-#define RX_PIN 0// todo fill in
-#define TX_PIN 0// todo fill in
+#define RX_PIN 3// todo fill in
+#define TX_PIN 2// todo fill in
 
+// Packet type defs
+#define TESTING_PKT 0b0000
+#define LEGACY_PKT  0b0001  
+#define CAR2024_PKT 0b0010
+#define CAR2025_PKT 0b0011
+
+// Error type defs
+#define NO_ERR        0b0000
+#define CRIT_ERR1     0b0001 
+#define CRIT_ERR2     0b0010 
+#define CRIT_ERR3     0b0011 
+#define CRIT_ERR4     0b0100 
+#define NON_CRIT_ERR1 0b0101 
+
+// other 
+#define PKT_LEN 29
+// #define SEND_OVER_SERIAL
 // gps stuff
 TinyGPSPlus gps; // the TinyGPS++ object
+
+EspSoftwareSerial::UART gpsSerial;
+
 // todo: use proper serial connection setup ---> USE "Serial1"  
 // SoftwareSerial gpsSerial(RX_PIN, TX_PIN); // the serial interface to the GPS module
 
@@ -36,6 +58,58 @@ int num_sent = 0;
 
 SX1262 radio = new Module(LORA_CS, LORA_DIO1, LORA_NRST, LORA_BUSY);
 
+float get_temp(){
+  float raw_temp = 0;
+	temp_sensor_read_celsius(&raw_temp);
+	raw_temp = (raw_temp * 9.0/5.0) + 32;
+  return raw_temp;
+}
+
+void insert_float(uint8_t* pkt, float num, int index){
+  uint8_t byteArray[sizeof(float)];
+	memcpy(byteArray, &num, sizeof(float));
+  
+  uint8_t bigend_array[4] = {byteArray[3], byteArray[2], byteArray[1], byteArray[0]};
+  for (int i = index; i < sizeof(float); i++) {
+		pkt[i] = bigend_array[i - index];
+	}
+}
+
+// function only called with valid gps info
+void create_packet(uint8_t* pkt){
+  pkt[0] = (TESTING_PKT << 4) | NO_ERR; // TYPE & ERROR 
+
+  pkt[1] = PKT_LEN - 1; //length of pkt in bytes
+
+  pkt[2] = (uint8_t) gps.course.deg(); // gps heading
+
+  //battery voltage (float)... spoofed val
+  pkt[3] = 0;
+  pkt[4] = 0;
+  pkt[5] = 0;
+  pkt[6] = 0;
+
+  // temp
+  insert_float(pkt, get_temp(), 7);  
+
+  pkt[11] = (uint8_t) (10 * gps.speed.mph()); // throttle input spoofed w scaled velocity
+  
+  insert_float(pkt, 1/(10 * gps.speed.mph()), 12); // brake pressure, inverse scaled velocity
+
+  insert_float(pkt, gps.speed.mph(), 16); // wheelspeed, replaced w velocity
+
+  insert_float(pkt, gps.location.lat(), 20);
+
+  insert_float(pkt, gps.location.lng(), 24);
+}
+
+void print_pkt(uint8_t* pkt){
+  Serial.print("Packet with bytes: \n");
+
+  for (int i = 0; i < PKT_LEN; i++){
+    Serial.println(pkt[i], HEX);
+  }
+}
 // Note: remember to remove Serial.print() statements before going to production on the car; could fail and reset the device. This is because Serial.print() uses interrupt to read data, 
 // but in an ISR, all other interrupts are suspended, and therefore the operation fails.
 
@@ -87,12 +161,43 @@ void setup() {
       error_message("Current limit intialization failed", state);
   }
   // gps stuff
-  // gpsSerial.begin(9600);
+  gpsSerial.begin(9600, SWSERIAL_8N1, RX_PIN, TX_PIN, false);  // Default baud of NEO-6M GPS module is 9600
+  if (!gpsSerial) { // If the object did not initialize, then its configuration is invalid
+    Serial.println("Invalid EspSoftwareSerial pin configuration, check config"); 
+    while (1) { // Don't continue with invalid configuration
+      delay (1000);
+    }
+  } 
+
   Serial.println(F("Arduino - GPS module"));
+
+  //config temp sensor
+  temp_sensor_config_t temp_sensor = TSENS_CONFIG_DEFAULT();
+  temp_sensor.dac_offset = TSENS_DAC_L2;  // TSENS_DAC_L2 is default; L4(-40°C ~ 20°C), L2(-10°C ~ 80°C), L1(20°C ~ 100°C), L0(50°C ~ 125°C)
+  temp_sensor_set_config(temp_sensor);
+  temp_sensor_start();
 }
 
 void loop() {
   // Check if there's data available on the serial port
+  if (gpsSerial.available() > 0){
+    if(gps.encode(gpsSerial.read())){
+      if (gps.location.isValid() && gps.speed.isValid() && gps.course.isValid()){
+        uint8_t my_pkt[PKT_LEN];
+        create_packet(my_pkt);
+
+        print_pkt(my_pkt);
+        //todo: tx packet
+      } else{
+        Serial.println("gps params not valid");
+      }
+    }
+  }
+  
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+    Serial.println(F("No GPS data received: check wiring"));
+
+  #ifdef SEND_OVER_SERIAL
   if (Serial.available() > 0) {
     String tx_payload = Serial.readString(); // Read data from the serial port
     // You may want to add some validation or processing here
@@ -120,6 +225,8 @@ void loop() {
       Serial.println(state);
     }
   }
+  #endif
+
   //  else if(gpsSerial.available() > 0){
   //   if (gps.encode(gpsSerial.read())) {
   //     if (gps.location.isValid()) {
